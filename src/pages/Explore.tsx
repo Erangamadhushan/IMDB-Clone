6e5da67c74b383b91685b1c1dfc2cd61d012f75a
Erangamadhushan <erangamadhushann956@gmail.com>
2026-01-02 13:51:17 +0530

    Update Footer and Explore components for improved functionality and layout
    - Changed footer links from "#" to "/"
    - Refactored newsletter subscription form to prevent default submission and added alert
    - Enhanced Explore component's layout and error handling
    - Updated Favorite component's styling for consistent appearance

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,108 +1,113 @@
-import IMDBNavbar from '../components/Navbar'
-import { Footer } from '../components/Footer'
-import { useState, useEffect } from 'react';
+import IMDBNavbar from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { useState, useEffect } from "react";
 import { searchMovies, getPopularMovies } from "../services/api";
-import MovieCard from '../components/ui/Home/MovieCard';
+import MovieCard from "../components/ui/Home/MovieCard";
 
 function Explore() {
-    const [movies, setMovies] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const loadPopularMovies = async () => {
-        try {
-            const popularMovies = await getPopularMovies();
-            setMovies(popularMovies);
-        } catch (err) {
-            console.error(err);
-            setError("Failed to load movies...");
-        } finally {
-            setLoading(false);
-        }
-        };
-
-        loadPopularMovies();
-    }, []); // ✅ run once
-
-    const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!searchQuery.trim()) {
-          const popularMovies = await getPopularMovies();
-          setMovies(popularMovies);
-          setError(null);
-          setLoading(false);
-          return;
-        }
-        if (loading) return;
-    
-        setLoading(true);
-        try {
-          const searchResults = await searchMovies(searchQuery);
-          setMovies(searchResults);
-          setError(null);
-        } catch (err) {
-          console.log(err);
-          setError("Failed to search movies...");
-        } finally {
-          setLoading(false);
-        }
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (err) {
+        console.error(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
     };
 
-    return (
-        <>
-            <IMDBNavbar />
-            <section className="explore-section w-full py-20 border-b-4 border-b-red-950 bg-linear-to-l from-red-950 via-red-800 to-red-950 text-white items-center px-4">
-                <div className="max-w-5xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2">
-                    <div>
-                        <h2 className="text-4xl font-bold my-4 text-center">Explore More Movies</h2>
-                    </div>
-                    
-                </div>
-                <div className="md:py-8 w-full box-border sm:py-8 py-4">
-                    <h2 className="text-3xl font-bold text-white text-center mb-4">
-                        {searchQuery
-                        ? `Search Results for "${searchQuery}"`
-                        : "Popular Movies"}
-                    </h2>
-                    <form
-                        onSubmit={handleSearch}
-                        className="max-w-[600px] mx-auto md:mb-8 flex gap-4 px-4 box-border sm:mb-8 mb-4"
-                    >
-                        <input
-                        type="text"
-                        placeholder="Search for movies..."
-                        className="flex-1 py-3 px-4 bg-yellow-400 rounded-full text-white text-base 
+    loadPopularMovies();
+  }, []); // ✅ run once
+
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) {
+      const popularMovies = await getPopularMovies();
+      setMovies(popularMovies);
+      setError(null);
+      setLoading(false);
+      return;
+    }
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <IMDBNavbar />
+      <section className="explore-section w-full py-20 border-b-4 border-b-red-950 bg-linear-to-l from-red-950 via-red-800 to-red-950 text-white items-center px-4">
+        <div className="max-w-5xl mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2">
+          <div>
+            <h2 className="text-4xl font-bold my-4 text-center">
+              Explore More Movies
+            </h2>
+          </div>
+        </div>
+        <div className="md:py-8 w-full box-border sm:py-8 py-4">
+          <h2 className="text-3xl font-bold text-white text-center mb-4">
+            {searchQuery
+              ? `Search Results for "${searchQuery}"`
+              : "Popular Movies"}
+          </h2>
+          <form
+            onSubmit={handleSearch}
+            className="max-w-[600px] mx-auto md:mb-8 flex gap-4 px-4 box-border sm:mb-8 mb-4"
+          >
+            <input
+              type="text"
+              placeholder="Search for movies..."
+              className="flex-1 py-3 px-4 bg-yellow-400 rounded-full text-white text-base 
                                     focus:outline-none focus:ring-2 focus:ring-yellow-400 placeholder:text-black/70"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                        />
-                        <button
-                        type="submit"
-                        className="py-3 px-6 bg-yellow-400 text-white rounded-xl font-medium whitespace-nowrap 
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="py-3 px-6 bg-yellow-400 text-white rounded-xl font-medium whitespace-nowrap 
                                     hover:bg-yellow-500 transition-colors"
-                        >
-                        Search
-                        </button>
-                    </form>
+            >
+              Search
+            </button>
+          </form>
 
-                    {error && <div className="error-message">{error}</div>}
+          {error && <div className="error-message">{error}</div>}
 
-                    {loading ? (
-                        <div className="loading">Loading...</div>
-                    ) : (
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4 w-full box-border">
-                        {movies.map((movie, index) => (
-                            <>
-                            
-                            <MovieCard movie={movie} key={index} onClick={() => setSelectMovie(true)} className="animate-spin" />
-                            </>
-                        ))}
-                        </div>
-                    )}
-                </div>
-                {/* <div >
+          {loading ? (
+            <div className="loading">Loading...</div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 p-4 w-full box-border">
+              {movies.map((movie, index) => (
+                <>
+                  <MovieCard
+                    movie={movie}
+                    key={index}
+                    onClick={() => setSelectMovie(true)}
+                    className="animate-spin"
+                  />
+                </>
+              ))}
+            </div>
+          )}
+        </div>
+        {/* <div >
                     {error && <div className="error-message">{error}</div>}
 
                     {loading ? (
@@ -115,10 +120,10 @@ function Explore() {
                     </div>
                     )}
                 </div> */}
-            </section>
-            <Footer />
-        </>
-    )
+      </section>
+      <Footer />
+    </>
+  );
 }
 
-export default Explore
+export default Explore;
--- src/pages/Favourite.tsx
+++ src/pages/Favourite.tsx
@@ -1,7 +1,7 @@
 import { useMovieContext } from "../context/MovieContext";
 import MovieCard from "../components/ui/Home/MovieCard";
 import { useState, useEffect } from "react";
-import { searchMovies } from "../services/api";
+//import { searchMovies } from "../services/api";
 import type { Movie } from "../types/Movie";
 
 function Favorite() {
@@ -19,7 +19,7 @@ function Favorite() {
   }, []);
 
   return (
-    <div className="favorites bg-linear-to-r from-red-950 via-red-800 to-red-950 p-8 w-full box-border">
+    <div className="favorites bg-linear-to-r from-red-950 via-red-800 to-red-950 p-8 w-full min-h-screen box-border">
       {/* Page Title */}
       <div>
         {/* Back button */}
